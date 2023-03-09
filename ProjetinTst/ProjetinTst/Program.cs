7f6e9bab0162113e4e532b7d80ebe4d505abab85
Pare-dao <thauanwictor05@gmail.com>
2023-03-09 14:21:36 -0400

    criando projeto tst

---
+++ ProjetinTst/ProjetinTst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetinTst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola mundo");
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
